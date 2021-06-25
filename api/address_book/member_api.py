731fbf1422b7fa8ad2eae62df33963cfcabfa0ff
yjlmobs@163.com <lewyuejian@163.com>
2021-06-25 15:06:41 +0800

    7. 通讯录管理 封装成员管理的api

--- api/address_book/member_api.py
+++ api/address_book/member_api.py
@@ -13,6 +13,7 @@ from api.client import ApiRequest
 class Member(ApiRequest):
 
     def create_member(self, token, userid, name, mobile, department):
+        """创建成员"""
         url = f'/cgi-bin/user/create?access_token={token}'
         data = {
             "userid": userid,
@@ -20,8 +21,54 @@ class Member(ApiRequest):
             "mobile" : mobile,
             "department": department
         }
-        req = self.post(url=url, data=data)
+        req = self.post(url=url, json=data)
         return req
+
+    def get_member(self, token, userid):
+        """读取成员"""
+        url = f'/cgi-bin/user/get?access_token={token}&userid={userid}'
+        req = self.get(url=url)
+        return req
+
+    def update_member(self, token, userid, name=None, mobile=None):
+        """更新成员"""
+        url = f'/cgi-bin/user/update?access_token={token}'
+        data = {
+            "userid": userid,
+            "name": name,
+            "mobile": mobile
+        }
+        req = self.post(url=url, json=data)
+        return req
+
+    def delete_member(self, token, userid):
+        """删除成员"""
+        url = f'/cgi-bin/user/delete?access_token={token}&userid={userid}'
+        req = self.get(url=url)
+        return req
+
+    def batch_delete_member(self, token, useridlist):
+        """批量删除成员"""
+        url = f'/cgi-bin/user/batchdelete?access_token={token}'
+        data = {
+            "useridlist": useridlist
+        }
+        req = self.post(url=url, json=data)
+        return req
+
+    def get_department_member_details(self, token, department_id, fetch_child=0):
+        """
+        获取部门成员详情
+        :param token:
+        :param department_id: 部门id
+        :param fetch_child: 1/0：是否递归获取子部门下面的成员
+        :return:
+        """
+        url = f'/cgi-bin/user/list?access_token={token}&department_id={department_id}&fetch_child={fetch_child}'
+
+        req = self.get(url)
+        return req
+
 
 if __name__ == '__main__':
     from api.tokens import WechatApi
@@ -31,5 +78,16 @@ if __name__ == '__main__':
 
     token = WechatApi().obtain_token(corpid,secret)
     req = Member()
-    #token = 'Yc66zreFu8eUYNyjEnHb5ow9iNF-t1DPKTfqFG0Qmw7FuZNYG9XaASCyrgdAJKfW2iWilg4T0DPkCSze6ruqW1IG5q_wgaIMnBq52juVKCmldzjJi2OC5Rp0wWFzSaTzg9NPkCMJDSvBqHy_LJ8MDvtlGPtp9YHby61i8oYyJhdO57saykP94YfIDOIzSm7kwD5JMpSm5Q8ROzhfZJD3_A'
-    print(req.create_member(token, "taikong", name="taikong", mobile="18144854881", department="1"))
+    # 创建成员
+    #print(req.create_member(token, "taikong", name="taikong", mobile="18144854883", department="1"))
+    # print(req.create_member(token, "taikong1", name="taikong1", mobile="18144854882", department="1"))
+    # 获取成员
+    #print(req.get_member(token, "taikong"))
+    # 更新成员
+    #print(req.update_member(token, userid="taikong", mobile="18144854889"))
+    # 删除成员
+    #print(req.delete_member(token, "taikong"))
+    # 批量删除成员
+    #print(req.batch_delete_member(token, ["taikong", "taikong1"]))
+    # 获取部门成员详情
+    #print(req.get_department_member_details(token, 1))
